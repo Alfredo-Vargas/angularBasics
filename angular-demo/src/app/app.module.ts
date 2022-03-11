a80d4a1c910d0345234593632fd27a5636a9d056
pacha <pacha@archlen.localdomain>
2022-03-11 17:46:17 +0100

    first two components created

---
+++ angular-demo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
+import { MysecondcomponentComponent } from './mysecondcomponent/mysecondcomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyfirstcomponentComponent,
+    MysecondcomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
