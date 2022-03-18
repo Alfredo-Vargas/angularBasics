27cfa2e61fe9cf02b72fead91e2402ab03e659a0
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-03-18 15:13:34 +0100

    2022-03-18 15:1334

---
+++ data-binding/src/app/assignment1/assignment1.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment1',
+  templateUrl: './assignment1.component.html',
+  styleUrls: ['./assignment1.component.css']
+})
+export class Assignment1Component implements OnInit {
+
+  Username: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  empty():boolean {
+    return (this.Username === "");
+  }
+
+  onClick():void {
+    this.Username = "";
+  }
+}
