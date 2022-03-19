4bbcc11c6ebc1d8355a9cf3d923b746d9a73fa4c
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-03-19 13:32:36 +0100

    half way assignment3

--- data-binding/src/app/assignment3/assignment3.component.ts
+++ data-binding/src/app/assignment3/assignment3.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Assignment3Component implements OnInit {
 
+  givenTitle: string = "";
+  givenAuthor: string = "";
+  givenGenre: string = "";
+  givenImage: string = "";
+
+  genreOutput = window.document.getElementById("genre-output");
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onTextEntry(event: Event): void {
+    // this.genreOutput = window.document.getElementById("genre-output");
+    if (this.givenGenre == "sciencefiction"){
+      // The line below does not work, why? <-- genreOtuput is NULL !!, but why?
+      // this.genreOutput.setAttribute("style", "color:red;");
+      this.genreOutput.classList.add("scifi");
+    }
+  }
 }
