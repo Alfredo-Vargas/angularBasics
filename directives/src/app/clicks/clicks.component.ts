9fff7798c5be7e95f5eb341e2bee1a6e6e668639
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-04-21 01:51:58 +0200

    lab7 assignment 1 done

--- directives/src/app/clicks/clicks.component.ts
+++ directives/src/app/clicks/clicks.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClicksComponent implements OnInit {
 
+  buttonCondition: boolean = false;
+  now: Date;
+  clicksLog: Array<Date> = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(event: Event): void {
+    this.buttonCondition = !this.buttonCondition;
+    this.now = new Date();
+    if (this.clicksLog.length < 4) {
+      this.clicksLog.push(this.now);
+    }
+    else {
+      this.moreThanFourClicks();
+    }
+  }
+  moreThanFourClicks(): void {
+
+    if (this.clicksLog.length < 4) {
+      this.clicksLog.push(this.now);
+    }
+  }
 }
