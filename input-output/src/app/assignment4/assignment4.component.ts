68f1ced5b0efe1f6119c9b5549758d31c6faa914
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-04-25 21:41:32 +0200

    lab8 completed!

--- input-output/src/app/assignment4/assignment4.component.ts
+++ input-output/src/app/assignment4/assignment4.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-assignment4',
@@ -8,13 +9,33 @@ import { Input } from '@angular/core';
 })
 export class Assignment4Component implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("Here the Constructor gets called");
+  }
 
   @Input() body: String = "Here is the content";
 
-  header: String = "Here is the header";
+  @ViewChild('cardHeaderReference') headerHtmlObject!: ElementRef;
 
-  ngOnInit(): void {
+  ngAfterViewChecked(){
+    console.log(this.headerHtmlObject.nativeElement.className);
   }
 
+  header: String = "Here is the card header";
+
+  ngOnInit(): void {
+    console.log("ngOnInit was called");
+  }
+
+  ngOnChanges(): void {
+    console.log("ngOnChanges was called!");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit was called!");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy was called!");
+  }
 }
