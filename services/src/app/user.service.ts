13a214b7ace7601177c3db3941e0f1998d5c2523
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-04-29 14:00:05 +0200

    2022-04-29 14:0005

--- services/src/app/user.service.ts
+++ services/src/app/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  users = [
+    {username: 'Wim', password: 'Dams'},
+    {username: 'Lars', password: 'Struyf'},
+    {username: 'Phaedra', password: 'Degreef'},
+  ];
+
   constructor() { }
+
+  getUsers(): Array<User> {
+    console.log('getUsers returns: ', this.users);
+    return this.users;
+  }
+
+  addUser(user: User): void {
+    this.user.push(user);
+    console.log('addUser results in:', this.users);
+  }
 }
